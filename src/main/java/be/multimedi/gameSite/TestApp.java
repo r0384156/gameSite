7b13b9cb5ac48059d7bcb6a8cffd313eeb2e1552
Tom Derekx <tom.derekx@vdabheverlee.intra>
2019-09-18 14:20:23 +0200

    Addhighscore finished + tested in testApp

--- src/main/java/be/multimedi/gameSite/TestApp.java
+++ src/main/java/be/multimedi/gameSite/TestApp.java
@@ -11,5 +11,19 @@ public class TestApp {
         for (GamersScore h : highscoreLijst) {
             System.out.println(h);
         }
+
+        // test voorbeeld gamers
+        GamersScore gamer1 = new GamersScore("Bert", "Wandelseck");
+        GamersScore gamer2 = new GamersScore("Jan", "Mast");
+
+        // test addHighscores
+        float tempTijd = 30;
+        int tempHighscore = 350;
+        gsdao.addHighscore(gamer1, tempHighscore, tempTijd);
+        tempTijd++;
+        tempHighscore++;
+        System.out.println("Highscore toegevoegd voor : " + gamer1.getGamersVoornaam() + " " + gamer1.getGamersNaam());
+        gsdao.addHighscore(gamer2, tempHighscore, tempTijd);
+        System.out.println("Highscore toegevoegd voor : " + gamer2.getGamersVoornaam() + " " + gamer2.getGamersNaam());
     }
 }

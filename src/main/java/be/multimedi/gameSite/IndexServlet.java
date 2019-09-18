4d7f0dfd61b0604043d91cdf3e266d58656d78fb
DESKTOP-ARU6II5\Rombout <r0384156@ucll.be>
2019-09-18 13:39:18 +0200

    index test

--- src/main/java/be/multimedi/gameSite/IndexServlet.java
+++ src/main/java/be/multimedi/gameSite/IndexServlet.java
@@ -25,7 +25,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
         String page=req.getParameter("page");
-        if(page!=null&&page.equals("table")) {
+        if(page!=null&&page.equals("highscores")) {
             req.getRequestDispatcher("highscores.jsp").forward(req, resp);
         }else {
             req.getRequestDispatcher("index.jsp").forward(req, resp);
